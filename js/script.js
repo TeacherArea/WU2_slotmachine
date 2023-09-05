3fe215092684a9fa4c80b8fae68ba17ad9659c83
TeacherArea <sewilius.berg@lbs.se>
2023-09-05 23:17:29 +0200

    added probability and function for emptying slots between rounds

--- js/script.js
+++ js/script.js
@@ -14,13 +14,46 @@ const images = [
   '../img/smiley7.jpg',
   '../img/smiley8.jpg',
   '../img/smiley9.jpg',
+  '../img/smiley10.jpg'
 ];
 
+function getRandomImage() {
+  const probabilities = [
+    { image: '../img/smiley1.jpg', probability: 0.35 },
+    { image: '../img/smiley2.jpg', probability: 0.35 },
+    { image: '../img/smiley3.jpg', probability: 0.35 },
+    { image: '../img/smiley4.jpg', probability: 0.35 },
+    { image: '../img/smiley5.jpg', probability: 0.35 },
+    { image: '../img/smiley6.jpg', probability: 0.35 },
+    { image: '../img/smiley7.jpg', probability: 0.2 },
+    { image: '../img/smiley8.jpg', probability: 0.2 },
+    { image: '../img/smiley9.jpg', probability: 0.1 },
+    { image: '../img/smiley10.jpg', probability: 0.1 }
+  ];
+
+  const random = Math.random();
+  let cumulativeProbability = 0;
+
+  for (const { image, probability } of probabilities) {
+    cumulativeProbability += probability;
+    if (random < cumulativeProbability) {
+      return image;
+    }
+  }
+
+  return probabilities[0].image;
+}
+
 function spinReels() {
+  //nollställer ronden, så att inte tidigare ronds bilder ska påverka
+  reels.forEach(reel => {
+    reel.innerHTML = '';
+  });
+
   reels.forEach((reel, index) => {
     setTimeout(() => {
-      const randomIndex = Math.floor(Math.random() * images.length);
-      reel.innerHTML = `<img class="slot-image" src="${images[randomIndex]}" alt="Smiley ${randomIndex + 1}">`;
+      const randomImage = getRandomImage();
+      reel.innerHTML = `<img class="slot-image" src="${randomImage}" alt="Image of a smiley">`;
       checkWin();
     }, index * 500);
   });
@@ -45,4 +78,3 @@ function checkWin() {
 spinButton.addEventListener('click', () => {
   spinReels();
 });
-
